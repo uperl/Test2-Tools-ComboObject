1ceb981899df38955a0675eebcb4ba22ddebc770
Graham Ollis <plicease@cpan.org>
2024-11-21 09:55:21 -0700

    Initial structure

---
+++ lib/Test2/Tools/ComboObject.pm
@@ -0,0 +1,11 @@
+use warnings;
+use 5.020;
+use experimental qw( postderef signatures );
+
+package Test2::Tools::ComboObject {
+
+  # ABSTRACT: Combine checks and diagnostics into a single test as an object
+}
+
+1;
+
